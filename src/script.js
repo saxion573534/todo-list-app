472f4f7a4e58011730e11a11101b57138fcb2bea
Abdullah Caglayan <573534@student.saxion.nl>
2025-05-22 23:47:02 +0200

    Tasks can now get created and added to the list

--- src/script.js
+++ src/script.js
@@ -1,6 +1,7 @@
 const tasksCompletedText = document.getElementById('tasks-completed');
 const form = document.querySelector('form');
 const taskInputField = document.getElementById('taskfield');
+const tasks = document.getElementById('tasks');
 
 function updateTasksCompleted(tasksCompleted, totalTasks) {
     if (tasksCompleted <= 0) {
@@ -14,6 +15,35 @@ function updateTasksCompleted(tasksCompleted, totalTasks) {
 
 function getTasksCompleted(tasks) {
   return tasks.filter(task => task.complete).length;
+}
+
+function createTask(title) {
+    // <div class="task">
+    //     <input class="checkbox" type="checkbox"/>
+    //     <p class="task-title">leetcode exercises</p>
+    //     <button class="close-task">X</button>
+    // </div>
+
+    const task = document.createElement('div');
+    task.className = 'task';
+
+    const checkbox = document.createElement('input');
+    checkbox.className = 'checkbox';
+    checkbox.type = 'checkbox';
+
+    const taskTitle = document.createElement('p');
+    taskTitle.className = 'task-title';
+    taskTitle.textContent = title;
+
+    const closeBtn = document.createElement('button');
+    closeBtn.className = 'close-task';
+    closeBtn.textContent = 'X';
+
+    task.appendChild(checkbox);
+    task.appendChild(taskTitle);
+    task.appendChild(closeBtn);
+
+    tasks.appendChild(task);
 }
 
 form.addEventListener('submit', (event) => {
@@ -35,7 +65,7 @@ fetch('../tasks.json')
             const content = task.content
             const complete = task.complete
             const created = task.created
-            // TODO: create task on screen here...
+            createTask(content)
             console.log(content);
         }
         updateTasksCompleted(getTasksCompleted(tasks), tasks.length);
